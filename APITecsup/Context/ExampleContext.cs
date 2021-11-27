04383b1e94ddb67c8cbea0336d806a47ce9b6e63
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2021-11-26 19:36:21 -0500

    API EntityFramework

---
+++ APITecsup/Context/ExampleContext.cs
@@ -0,0 +1,21 @@
+﻿using APITecsup.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace APITecsup.Context
+{
+    public class ExampleContext : DbContext
+    {
+        public ExampleContext() : base("name = MyContextDB")
+        {
+
+        }
+
+        public DbSet<Person> People  { get; set; }
+
+
+    }
+}
--- APITecsup/Models/Person.cs
+++ APITecsup/Models/Person.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +10,9 @@ namespace APITecsup.Models
     public class Person
     {
         public int PersonID { get; set; }
+        [StringLength(100)]
         public string LastName { get; set; }
+        [StringLength(100)]
         public string FirstName { get; set; }
     }
 }
